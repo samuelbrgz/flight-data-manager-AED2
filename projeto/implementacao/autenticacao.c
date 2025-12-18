3a07234a5c8cbf10618ced18c92dc38a6d6b11e6
Luciano Tadeu <lucianotssjr@gmail.com>
2025-12-18 19:02:45 -0400

    Correção e adaptação para o novo template do projeto

--- projeto/implementacao/autenticacao.c
+++ projeto/implementacao/autenticacao.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include "login.h"
+#include "autenticacao.h"
 
 bool login(){
+
+    bool verificar = false; // variável de apoio para identificar se digitou o login correto
 
     Usuario digitado;
     Usuario arquivo;
@@ -15,7 +17,7 @@ bool login(){
     fgets(digitado.senha, sizeof(digitado.senha), stdin);
     digitado.senha[strcspn(digitado.senha, "\r\n")] = '\0';
 
-    FILE *fp = fopen("usuarios.csv", "r");
+    FILE *fp = fopen("..\\dados\\usuarios.csv", "r"); // arquivo csv está em outra pasta
     if (!fp) {
         printf("Erro ao abrir o arquivo para leitura.\n");
         return false;
@@ -23,15 +25,23 @@ bool login(){
     
     char linha[120];
 
-    while (fgets(linha, sizeof(linha), fp)) {
+    while (fgets(linha, sizeof(linha), fp)) { // laço de repetição que escaneia linha a linha do .csv e compara com o login e senha digitados
         sscanf(linha, "%49[^,],%49s", arquivo.login, arquivo.senha);
 
         if (strcmp(digitado.login, arquivo.login) == 0 && strcmp(digitado.senha, arquivo.senha) == 0) {
-            return true;
-            break;
+            verificar = true;
         }
     }
 
+    if (verificar == false)
+    {
+        printf("\nLogin ou senha incorretos...\n");
+    }
+    else
+    {
+        printf("\n%s entrou no sistema!\n", digitado.login);
+    }
+
     fclose(fp);
-    return false;
+    return verificar; // para poder finalizar o laço de repetição no programa principal.
 }
