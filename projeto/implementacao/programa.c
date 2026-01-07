60bb0f3d890f771b9ac37fea73c726f4709acf49
Samuel <samuelvborgess@gmail.com>
2026-01-07 16:52:28 -0400

    Implementacao da funcionalidade de cadastro de viagens (RF0003)

--- projeto/implementacao/programa.c
+++ projeto/implementacao/programa.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include "autenticacao.h"
 #include "logs.h"
+#include "dados.h"
 
 int main(void)
 {
-
+    No *lista = NULL;
+    carregarDados(&lista); // carrega os dados do arquivo base e insere na lista
     int escolha = 0;
 
     Validar usuario; // puxar o usuário da sessão por aqui
@@ -23,7 +25,7 @@ int main(void)
         printf("\n=================================\n");
         printf("           MENU PRINCIPAL        \n");
         printf("=================================\n");
-        printf(" 1 - Cadastrar novo item\n");
+        printf(" 1 - Cadastrar viagem\n");
         printf(" 2 - Listar registros\n");
         printf(" 3 - Pesquisar registro\n");
         printf(" 4 - Editar registro\n");
@@ -31,15 +33,20 @@ int main(void)
         printf(" 6 - Encerrar o programa\n");
         printf("=================================\n");
         printf("Escolha uma opcao: ");
-        scanf("%d", &escolha);
 
-        while (getchar() != '\n')
-            ;
+        if (scanf("%d", &escolha) != 1)
+        {
+            printf("Opcao invalida.\n");
+            while (getchar() != '\n')
+                ; // limpa buffer
+            continue;
+        }
+        getchar();
 
         switch (escolha)
         {
         case 1:
-            // cadastrar
+            cadastrarViagem(&lista);
             break;
 
         case 2:
